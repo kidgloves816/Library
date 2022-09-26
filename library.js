926b16c927c7666c6f27a7e3748fae547a9e8a95
Brynn <durantbrynn@gmail.com>
2022-09-26 18:10:34 -0500

    Adjusted library function

--- library.js
+++ library.js
@@ -8,6 +8,6 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    let book = Book(title, author, pages, read);
+    let book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
