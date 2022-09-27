9ee48b5bcdb92e62d24d5c2a54466112f4f0c063
Brynn <durantbrynn@gmail.com>
2022-09-27 18:47:31 -0500

    set up card for books

--- library.js
+++ library.js
@@ -10,4 +10,11 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
+    return book;
+}
+
+const div = document.createElement('div');
+function createBookCard() {
+    addBookToLibrary();
+   div.appendChild(book.title);
 }
